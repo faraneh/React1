fe5ffaeda8dd0f0f97ae170d891c47b0d53f41e9
Farane Jn <>
2021-06-04 22:11:57 -0400

    Third

--- src/container/secondPart/firstRow/firstRow.jsx
+++ src/container/secondPart/firstRow/firstRow.jsx
@@ -10,7 +10,7 @@ const firstRow = () => {
     }
     return ( 
         <div style={classP}>
-            <h2 style={{marginBottom: 30, fontWeight: '700'}}>WHO we are?</h2>
+            <h2 style={{marginBottom: 30, fontWeight: '700'}}>WHO WE are?</h2>
             <p style={{color: 'lightgrey', fontSize: '1.19rem', fontWeight: '300'}}>According to the National Oceanic and Atmospheric Administration, Ted, Scambos, NSIDClead scentist, puts the potentially record low maximum sea ice extent tihs year down to low ice extent in the Pacific and a late drop in ice extent in the Barents Sea.</p>
         </div>
      );
